6d0b8896c26f5459b38f0929b8913719c1cfb575
jlebiannic <johann.lebiannic@soprasteria.com>
2023-04-02 16:58:51 +0200

    refactor

--- src/routes/todoList/+page.svelte
+++ src/routes/todoList/+page.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
-	import TodoList from "./component/TodoList.svelte";
-	import { Todo } from "./model/Todo";
-
-	let libelleTodo = "";
-	let todos: Todo[] = [];
-	function ajouteTodo() {
-		todos = [...todos, new Todo(todos.length, libelleTodo)];
-	}
+	import Todos from "./component/Todos.svelte";
 </script>
 
-<input bind:value={libelleTodo} /><button on:click={ajouteTodo}>Ajouter</button>
-<TodoList {todos} />
+<Todos />
---
+++ src/routes/todoList/component/Todos.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Todo } from "../model/Todo";
+	import TodoList from "./TodoList.svelte";
+
+	let libelleTodo = "";
+	let todos: Todo[] = [];
+	function ajouteTodo() {
+		todos = [...todos, new Todo(todos.length, libelleTodo)];
+	}
+</script>
+
+<input bind:value={libelleTodo} /><button on:click={ajouteTodo}>Ajouter</button>
+<TodoList {todos} />
